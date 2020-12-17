ea8fce7e53d7ac87486eaa82362109db98d338e3
srinathstuycs <smahankali10@stuy.edu>
2020-12-17 10:47:31 -0500

    Added swap()

--- Sorts.java
+++ Sorts.java
@@ -1,10 +1,19 @@
 import java.util.Arrays;
 public class Sorts{
+  public static void swap(int[] array, int a) {
+    if (a >= 0 && a+1 < array.length) {
+      if (array[a] > array[a+1]) {
+        int store = array[a];
+        array[a] = array[a+1];
+        array[a+1] = store;
+      }
+    }
+  }
   /**Bubble sort of an int array.
   *Upon completion, the elements of the array will be in increasing order.
   *@param data  the elements to be sorted.
   */
   public static void bubbleSort(int[] data) {
-    
+
   }
 }
