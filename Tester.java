b191380628c9c69a9b2654c3106a4c3aa612fd2e
srinathstuycs <smahankali10@stuy.edu>
2020-12-20 21:30:15 -0500

    Tested insertionSort

--- Tester.java
+++ Tester.java
@@ -8,6 +8,7 @@ public class Tester {
       System.out.println(Arrays.toString(array));
     }
     //Test bubbleSort
+    System.out.println("Testing bubbleSort");
     int[] array2 = {10,11,13,8,4,16};
     Sorts.bubbleSort(array2);
     System.out.println(Arrays.toString(array2));
@@ -18,6 +19,7 @@ public class Tester {
     Sorts.bubbleSort(almostSorted);
     System.out.println(Arrays.toString(almostSorted));
     //Test selectionSort
+    System.out.println("Testing selectionSort");
     int[] array3 = {10,11,13,8,4,16};
     Sorts.selectionSort(array3);
     System.out.println(Arrays.toString(array3));
@@ -26,6 +28,21 @@ public class Tester {
     int[] almostSorted2 = {1,2,3,4,6,5};
     Sorts.selectionSort(almostSorted2);
     System.out.println(Arrays.toString(almostSorted2));
+    //Test insertionSort
+    System.out.println("Testing insertionSort");
+    int[] array4 = {10,11,13,8,4,16};
+    int[] almostSorted3 = {1,2,3,4,6,5};
+    Sorts.insertionSort(array4);
+    Sorts.insertionSort(sortedArray);
+    Sorts.insertionSort(almostSorted3);
+    System.out.println(Arrays.toString(array4));
+    System.out.println(Arrays.toString(sortedArray));
+    System.out.println(Arrays.toString(almostSorted3));
+    //More tests for insertionSort
+    System.out.println("More tests for insertionSort");
+    int[] wackyArray = {1,3,4,9,5,11,10,2,8,7,6};
+    Sorts.insertionSort(wackyArray);
+    System.out.println(Arrays.toString(wackyArray));
   }
 
 }
