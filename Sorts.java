97fbfd854b5af48310e63e9368a8e1461f74e0ae
srinathstuycs <smahankali10@stuy.edu>
2020-12-20 20:01:27 -0500

    Removed main method

--- Sorts.java
+++ Sorts.java
@@ -32,20 +32,5 @@ public class Sorts{
     }
   }
 
-  /* public static void main(String[] args) {
-    int[] array = {9,8,7,6,5,4,3,2,1,0};
-    for (int i = 0; i < array.length - 1; i++) {
-      swap(array, i);
-      System.out.println(Arrays.toString(array));
-    }
-    int[] array2 = {10,11,13,8,4,16};
-    bubbleSort(array2);
-    System.out.println(Arrays.toString(array2));
-    int[] sortedArray = {1,2,3,4,5,6};
-    bubbleSort(sortedArray);
-    System.out.println(Arrays.toString(sortedArray));
-    int[] almostSorted = {1,2,3,4,6,5};
-    bubbleSort(almostSorted);
-    System.out.println(Arrays.toString(almostSorted));
-  } */
+  
 }
