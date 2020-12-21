4732e16f5e2ac7dd8f992b5a6a4e160c2987e90e
srinathstuycs <smahankali10@stuy.edu>
2020-12-21 14:06:06 -0500

    Added usage information

--- Driver.java
+++ Driver.java
@@ -2,58 +2,61 @@ import java.util.Arrays;
 public class Driver{
 
   public static void main(String[]args){
-    //args 0 must be size of the Array
-    //args 1 must be "bubble," "selection," or "test" (unless you add more)
-    int[] randData = new int[Integer.parseInt(args[0])];
-
-    for(int i = 0 ; i < randData.length; i++){
-      randData[i] =(int)(Math.random()*10000);
+    if(args.length < 2){
+      System.out.println("usage: java Driver SIZE ALGO [STYLE]");
+      System.out.println("supported ALGO: bubble,selection,insertion,test");
+      System.out.println("supported STYLE: random,equal,sorted,reversed");
     }
+    else{
+      int[] randData = new int[Integer.parseInt(args[0])];
 
-    if(args[1].equals("bubble")){
-      Sorts.bubbleSort(randData);
-    }
-
-    if(args[1].equals("selection")){
-      Sorts.selectionSort(randData);
-    }
-
-    if(args[1].equals("insertion")){
-      Sorts.insertionSort(randData);
-    }
-
-    if(args[1].equals("test")){
-      //test bubble
-      int[] randDataBubble = Arrays.copyOf(randData,randData.length);
-      Arrays.sort(randData);
-      Sorts.bubbleSort(randDataBubble);
-
-      if( Arrays.equals(randData,randDataBubble)){
-        System.out.println("Bubble Correct!");
-      }else{
-       System.out.println("Bubble Not Correct!!!!!!!!!11oneeleven");
+      for(int i = 0 ; i < randData.length; i++){
+        randData[i] =(int)(Math.random()*10000);
       }
 
-      //test selection
-      int[] randDataSelection = Arrays.copyOf(randData,randData.length);
-      Arrays.sort(randData);
-      Sorts.selectionSort(randDataSelection);
-
-      if( Arrays.equals(randData,randDataSelection)){
-        System.out.println("Selection Correct!");
-      }else{
-       System.out.println("Selection Not Correct!!!!!!!!!11oneeleven");
+      if(args[1].equals("bubble")){
+        Sorts.bubbleSort(randData);
       }
+      else if(args[1].equals("selection")){
+        Sorts.selectionSort(randData);
+      }
+      else if(args[1].equals("insertion")){
+        Sorts.insertionSort(randData);
+      }
+
+      if(args[1].equals("test")){
+        //test bubble
+        int[] randDataBubble = Arrays.copyOf(randData,randData.length);
+        Arrays.sort(randData);
+        Sorts.bubbleSort(randDataBubble);
+
+        if( Arrays.equals(randData,randDataBubble)){
+          System.out.println("Bubble Correct!");
+        }else{
+          System.out.println("Bubble Not Correct!!!!!!!!!11oneeleven");
+        }
+
+        //test selection
+        int[] randDataSelection = Arrays.copyOf(randData,randData.length);
+        Arrays.sort(randData);
+        Sorts.selectionSort(randDataSelection);
+
+        if( Arrays.equals(randData,randDataSelection)){
+          System.out.println("Selection Correct!");
+        }else{
+          System.out.println("Selection Not Correct!!!!!!!!!11oneeleven");
+        }
 
       //test insertion
-      int[] randDataInsertion = Arrays.copyOf(randData,randData.length);
-      Arrays.sort(randData);
-      Sorts.insertionSort(randDataInsertion);
+        int[] randDataInsertion = Arrays.copyOf(randData,randData.length);
+        Arrays.sort(randData);
+        Sorts.insertionSort(randDataInsertion);
 
-      if( Arrays.equals(randData,randDataInsertion)){
-        System.out.println("Insertion Correct!");
-      }else{
-       System.out.println("Insertion Not Correct!!!!!!!!!11oneeleven");
+        if( Arrays.equals(randData,randDataInsertion)){
+          System.out.println("Insertion Correct!");
+        }else{
+          System.out.println("Insertion Not Correct!!!!!!!!!11oneeleven");
+        }
       }
     }
   }
