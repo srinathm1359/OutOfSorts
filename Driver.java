815dab69c37b52ed3813e4a15eded9d255b378a0
srinathstuycs <smahankali10@stuy.edu>
2020-12-23 23:35:34 -0500

    Changed spacing of code

--- Driver.java
+++ Driver.java
@@ -11,19 +11,19 @@ public class Driver{
 
       int[] randData = new int[Integer.parseInt(args[0])];
 
-      if(args.length < 3 || args[2].equals("random")){
+      if(args.length < 3 || args[2].equals("random")) {
         for(int i = 0; i < randData.length; i++){
           randData[i] =(int)(Math.random()*10000);
         }
-      }else if(args[2].equals("equal")){
+      }else if(args[2].equals("equal")) {
         for (int i = 0; i < randData.length; i++){
           randData[i] = 0;
         }
-      }else if(args[2].equals("sorted")){
+      }else if(args[2].equals("sorted")) {
 	      for (int i = 0; i < randData.length; i++){
           randData[i] = i;
         }
-      }else if(args[2].equals("reversed")){
+      }else if(args[2].equals("reversed")) {
 	      for (int i = 0; i < randData.length; i++){
           randData[i] = -i;
         }
